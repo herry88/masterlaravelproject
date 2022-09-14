c121549bc356f165ceba1903086f39d8c3cd4fd4
Herry Prasetyo <herry081288@gmail.com>
2022-09-14 18:07:38 +0700

    menu

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,25 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+        <a href="index.html">Stisla</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+        <a href="index.html">St</a>
+    </div>
+
+    <ul class="sidebar-menu">
+        <li class="menu-header">Menu Utama</li>
+        <li class="nav-link">
+            <a href="#">Dashboard</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown">
+                <i class="fas fa-fire"></i><span>Data Master</span></a>
+            <ul class="dropdown-menu">
+                <li><a href="#" class="nav-link">Data Kategori</a></li>
+            </ul>
+
+        </li>
+    </ul>
+
+
+</aside>
